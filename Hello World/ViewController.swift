8e4b6afe19f32a9dc13a087ff543a969ea3446fc
Hongyi Guo <guohongyi1991@gmail.com>
2016-02-04 20:36:24 -0600

    First Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Hongyi Guo on 2/4/16.
+//  Copyright © 2016 Hongyi Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
